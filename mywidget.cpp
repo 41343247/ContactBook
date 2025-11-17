2b7b9a99e54890222773a0f6f0366dd633e11658
41343247 <41343247@nfu.edu.tw>
2025-11-17 16:19:58 +0800

    補

--- mywidget.cpp
+++ mywidget.cpp
@@ -1,21 +1,21 @@
 #include "mywidget.h"
-#include "ui_mywidget.h"
-#include <QFile>
 #include <QDebug>
+#include <QFile>
 #include <QFileDialog>
+#include "ui_mywidget.h"
 
-QString mFilename="C:/test/myfile.txt";
+QString mFilename = "C:/test/myfile.txt";
 
-void Write(QString Filename,QString str)
+void Write(QString Filename, QString str)
 {
     QFile mFile(Filename);
-    if(!mFile.open(QFile::WriteOnly | QFile::Text)){
-        qDebug()<<"could not open file for write";
+    if (!mFile.open(QFile::WriteOnly | QFile::Text)) {
+        qDebug() << "could not open file for write";
         return;
     }
 
     QTextStream out(&mFile);
-    out<<str;
+    out << str;
     mFile.flush();
     mFile.close();
 }
@@ -27,7 +27,8 @@ MyWidget::MyWidget(QWidget *parent)
     ui->setupUi(this);
     ui->tableWidget->setColumnCount(4);
     QStringList ColTotle;
-    ColTotle<<QStringLiteral("學號")<<QStringLiteral("班級")<<QStringLiteral("姓名")<<QStringLiteral("電話");
+    ColTotle << QStringLiteral("學號") << QStringLiteral("班級") << QStringLiteral("姓名")
+             << QStringLiteral("電話");
     ui->tableWidget->setHorizontalHeaderLabels(ColTotle);
 }
 
@@ -38,33 +39,32 @@ MyWidget::~MyWidget()
 
 void MyWidget::on_pushButton_clicked()
 {
-    QTableWidgetItem *col1,*col2,*col3,*col4;
-    col1=new QTableWidgetItem(QString(ui->lineEdit->text()));
-    col2=new QTableWidgetItem(QString(ui->lineEdit_2->text()));
-    col3=new QTableWidgetItem(QString(ui->lineEdit_3->text()));
-    col4=new QTableWidgetItem(QString(ui->lineEdit_4->text()));
+    QTableWidgetItem *col1, *col2, *col3, *col4;
+    col1 = new QTableWidgetItem(QString(ui->lineEdit->text()));
+    col2 = new QTableWidgetItem(QString(ui->lineEdit_2->text()));
+    col3 = new QTableWidgetItem(QString(ui->lineEdit_3->text()));
+    col4 = new QTableWidgetItem(QString(ui->lineEdit_4->text()));
 
     ui->tableWidget->insertRow(ui->tableWidget->rowCount());
 
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,0,col1);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,1,col2);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,2,col3);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,3,col4);
+    ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 0, col1);
+    ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 1, col2);
+    ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 2, col3);
+    ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, 3, col4);
 }
 
 void MyWidget::on_pushButton_2_clicked()
 {
-    QString saveFile="";
-    mFilename=QFileDialog::getSaveFileName(this,"匯出",".");
-    for(int i=0;i<ui->tableWidget->rowCount();i++){
-        for(int j=0;j<ui->tableWidget->columnCount();j++){
-            saveFile+=ui->tableWidget->item(i,j)->text()+",";
+    QString saveFile = "";
+    mFilename = QFileDialog::getSaveFileName(this, "匯出", ".");
+    for (int i = 0; i < ui->tableWidget->rowCount(); i++) {
+        for (int j = 0; j < ui->tableWidget->columnCount(); j++) {
+            saveFile += ui->tableWidget->item(i, j)->text() + ",";
         }
-        saveFile+="\n";
+        saveFile += "\n";
     }
-    Write(mFilename,saveFile);
+    Write(mFilename, saveFile);
 }
-
 
 void MyWidget::on_pushButton_3_clicked()
 {
@@ -102,11 +102,8 @@ void MyWidget::on_pushButton_3_clicked()
     file.close();
 }
 
-
-
 void MyWidget::on_pushButton_4_clicked()
 {
     on_pushButton_2_clicked();
     close();
 }
-
